1ebb0743010c463e949c17eda180051b27f3c11f
harshmohite4 <mohiteharsh639@gmail.com>
2025-09-24 01:20:18 +0530

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         secure: false,
       },
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:4000',
         changeOrigin: true,
         secure: false,
       },
